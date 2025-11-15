91655b6ef4d98045206c464cdf1405c1b0d28e2a
DinhThanhVy <vy.dtt.67khmt@ntu.edu.vn>
2025-11-16 01:24:05 +0700

    Create Thongtinsinhvien.py

---
+++ Thongtinsinhvien.py
@@ -0,0 +1,10 @@
+# Khai báo thông tin sinh viên
+ho_va_ten="Đinh Thị Thanh Vy"
+tuoi=18
+diem_tb=7.0
+trang_thai="đậu"
+#in ra thông tin
+print("Họ và tên:",ho_va_ten)
+print("Tuổi:",tuoi)
+print("Điểm trung bình:",diem_tb)
+print("Trạng thái:",trang_thai)
