942dedd4cf572c6769d3b2983d18dac299bb914e
DinhThanhVy <vy.dtt.67khmt@ntu.edu.vn>
2025-11-16 03:08:55 +0700

    tính tiền tip với 15%

---
+++ Tinhtientip.py
@@ -0,0 +1,8 @@
+tong_hoa_don=float(input("Nhập số tiền tổng hóa đơn: "))
+phan_tram_boa=0.15 #15%
+tien_boa=(tong_hoa_don*phan_tram_boa)
+thanh_toan_cuoi_cung=(tong_hoa_don+tien_boa)
+print("Tổng hóa đơn: ",tong_hoa_don)
+print("Phần trăm boa: ",phan_tram_boa)
+print("Tiền boa: ",tien_boa)
+print("Phải thanh toán: ",thanh_toan_cuoi_cung)
