fc2040c9a129fb0b1fb6272db6db63401066701c
DinhThanhVy <vy.dtt.67khmt@ntu.edu.vn>
2025-11-16 11:31:32 +0700

    nhập giờ,phút,giây và tính tổng số giây

---
+++ Tinhthoigian.py
@@ -0,0 +1,6 @@
+#Nhập giờ,phút,giây
+gio=float(input("Nhập số giờ :"))
+phut=float(input("Nhập số phút :"))
+giay=float(input("Nhập số giây :"))
+tong_so_giay=gio*3600+phut*60+giay
+print("Tổng số giây là:",tong_so_giay)
