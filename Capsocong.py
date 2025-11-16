225006b7caa8fdc6c4c55cd97ce1b855ec2b4438
DinhThanhVy <vy.dtt.67khmt@ntu.edu.vn>
2025-11-17 00:05:54 +0700

    tính cấp số cộng với công sai d

---
+++ Capsocong.py
@@ -0,0 +1,7 @@
+a1=int(input("Nhập số hạng đầu(a1):"))
+d=int(input("Nhập công sai(d):"))
+n=int(input("Nhập số lượng số hạng(n):"))
+so_hang_cuoi= a1+(n-1)*d
+Sn=n*(a1+so_hang_cuoi)/2
+print("Tổng cấp số cộng=",Sn)
+                
