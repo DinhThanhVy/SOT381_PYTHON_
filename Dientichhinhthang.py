d2b85183b47a9263c16f946041f2654217cac8ed
DinhThanhVy <vy.dtt.67khmt@ntu.edu.vn>
2025-11-16 23:42:55 +0700

    tính diện tích hình thang

---
+++ Dientichhinhthang.py
@@ -0,0 +1,5 @@
+day_lon=float(input("Nhập đáy lớn(m):"))
+day_be=float(input("Nhập đáy bé(m):"))
+chieu_cao=float(input("Nhập chiều cao(m):"))
+S=1/2*(day_lon+day_be)*chieu_cao
+print(f"Diện tích hình thang là:{S:.2f}(m^2)")
