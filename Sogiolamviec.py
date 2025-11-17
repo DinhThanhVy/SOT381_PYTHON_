8e0b815f22cae56bd4f60189f9610ef5b2ed5d38
DinhThanhVy <vy.dtt.67khmt@ntu.edu.vn>
2025-11-17 08:16:39 +0700

    tính tổng số giờ làm việc

---
+++ Sogiolamviec.py
@@ -0,0 +1,4 @@
+h=float(input("Nhập số giờ làm việc mỗi ngày: "))
+so_ngay=int(input("Nhập số ngày làm việc trong tháng: "))
+tong_so_gio=h*so_ngay
+print("Tổng số giờ làm việc là: ",round(tong_so_gio,2))
