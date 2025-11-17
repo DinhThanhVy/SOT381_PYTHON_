7774d833692e5023f30400b5e0629caee5055477
DinhThanhVy <vy.dtt.67khmt@ntu.edu.vn>
2025-11-17 07:42:36 +0700

    hoán đổi giá trị hai số

---
+++ Hoandoigiatri.py
@@ -0,0 +1,9 @@
+a=float(input("Nhập số thứ nhất:"))
+b=float(input("Nhập số thứ hai:"))
+print("Trước khi hoán đổi:")
+print("a=",a)
+print("b=",b)
+a,b=b,a
+print("Sau khi hoán đổi:")
+print("a=",a)
+print("b=",b)
