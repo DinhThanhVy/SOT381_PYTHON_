6b0799aa47621bc6458c065951d72f296c8e052a
DinhThanhVy <vy.dtt.67khmt@ntu.edu.vn>
2025-11-17 08:30:23 +0700

    tính chu vi và diện tích hình chữ nhật

---
+++ Chuvivadientich_hcn.py
@@ -0,0 +1,6 @@
+d=float(input("Nhập chiều dài của hình chữ nhật: "))
+r=float(input("Nhập chiều rộng của hình chữ nhật: "))
+p=(d+r)*2
+s=d*r
+print("Chu vi hình chữ nhật= ",round(p,2))
+print("Diện tích hình chữ nhật= ",round(s,2))
