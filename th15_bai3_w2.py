a582207c288001cbd0db1342ab058c14486bab3e
DinhThanhVy <vy.dtt.67khmt@ntu.edu.vn>
2025-12-15 15:26:07 +0700

    Update th15_bai3_w2.py

--- th15_bai3_w2.py
+++ th15_bai3_w2.py
@@ -4,7 +4,7 @@ while True:
         giai_thua=1
         for i in range(1,n+1):
             giai_thua*=i
-        print(f"{n}={giai_thua}")
+        print(f"{n}!={giai_thua}")
         break
     else:
         print("Nhập lại số hợp lệ")
