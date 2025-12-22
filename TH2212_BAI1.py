691b315bb903a5967a1afadc7b9dd34cd329dcf9
DinhThanhVy <vy.dtt.67khmt@ntu.edu.vn>
2025-12-22 14:16:24 +0700

    Update TH2212_BAI1.py

--- TH2212_BAI1.py
+++ TH2212_BAI1.py
@@ -1,6 +1,8 @@
 d=float(input("Nhập chiều dài: "))
 r=float(input("Nhập chiều rộng: "))
-c=(d+r)*2
-s=d*r
-print(f"chu vi hcn là:{c:.2f}")
-print(f"diện tích hcn là:{s:.2f}")
+while d>=0.0 and r<=100.0:
+ c=(d+r)*2
+ s=d*r
+ print(f"chu vi hcn là:{c:.2f}")
+ print(f"diện tích hcn là:{s:.2f}")
+ break
