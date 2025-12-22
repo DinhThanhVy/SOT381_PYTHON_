9e0309b5fd35d24152fe0b8f84fec4a9d5465682
DinhThanhVy <vy.dtt.67khmt@ntu.edu.vn>
2025-12-22 14:45:27 +0700

    tìm số lớn nhất

---
+++ th2212_bai2.py
@@ -0,0 +1,9 @@
+a=float(input("Nhập vào giá trị a:"))
+b=float(input("Nhập vào giá trị b:"))
+c=float(input("Nhập vào giá trị c:"))
+max=a
+if b> max:
+    max=b
+if c>max:
+    max=c
+print("số lớn nhất là", max)
